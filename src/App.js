9cd8ef143ada907f7bfa5919c6c5888c06228005
junjiebalatero <junjiebalatero@gmail.com>
2022-10-16 17:32:57 +0800

    8 edit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,9 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch("/api/date");
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+
+
+  
   return (
     <main>
        <h2> First modifiy the. from junjie</h2>
