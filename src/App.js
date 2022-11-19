aa392d829ac8383c5e92569ff8afaa69ce6ae139
Margarito V. Balatero Jr <junjiebalatero@gmail.com>
2022-11-19 18:12:42 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4355,10 +4355,6 @@ if (txtValue.toUpperCase().indexOf(filter) > -1) {
 </script>
 <!-- SEARCH FUNCTINALITY - END-->
 
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3486" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3485" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3435" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3434" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
     </main>
   );
 }
