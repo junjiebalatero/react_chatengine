797171f2fbee3f978191c5866ef463fc598b820d
Margarito V. Balatero Jr <junjiebalatero@gmail.com>
2022-11-19 19:13:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,42 +5,13 @@ import "./App.css";
 function App() {  
   return (
     <main>
-
-<!-- Beginning of Backtrack -->
-<h3>Hotel California - Eagles - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/8x26sdlQzbc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<h3>Highway Star - Deep Purple - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/0hAOXM-YzAE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<br>
-<h3>Flordeluna - Santana - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/eVHsvYcJ9ks" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<br>
-<h3>Take It Easy - Eagles - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/BRGt6p0oUSI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<h3>Till There Was You - Beatles - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/wGCXzUxjips" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<h3>Temple Of The King- Rainbow - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/5Yl40QrBfms" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<h3>Just The Two Of Us - Grover Washington - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/bGh4Qvd_KVY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-<h3>Something - Beatles - Backtrack</h3>
-<div>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/axmc0PyGXHA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
+              <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3486" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <br />
+              <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3485" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <br />
+              <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3435" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <br />
+              <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3434" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
     </main>
   );
 }
