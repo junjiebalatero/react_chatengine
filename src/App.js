fded5da04ac6d58d9f8ad3e4212a59b502896492
Margarito V. Balatero Jr <junjiebalatero@gmail.com>
2023-02-07 23:29:36 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <main>
       <h3>mardzreal57 web3 website hosted for free by 4everland.org </h3>
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3486" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://riffvideo.4everland.store/1%20-%20simply_jessie_by_rex_smith.mp4" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <br />
       <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3485" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <br />
