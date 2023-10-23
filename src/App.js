b4aa7d6f18b50114209db441dc16956888165e58
junjiebalatero <junjiebalatero@gmail.com>
2023-10-23 22:41:20 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -6,15 +6,13 @@ function App() {
   return (
     <main>
       <h3>mardzreal57 web3 website hosted for free by 4everland.org </h3>
-      <iframe width="560" height="315" src="https://riffvideo.4everland.store/1%20-%20simply_jessie_by_rex_smith.mp4" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <br />
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3485" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <br />
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3435" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <br />
-      <iframe width="560" height="315" src="https://dwqte-viaaa-aaaai-qaufq-cai.raw.ic0.app/embed/5789-3434" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+      <iframe width="560" height="315" src="https://chocolate-cooperative-herring-689.mypinata.cloud/ipfs/QmU71yCyMg95f4HcBFL293DZphmJRtmyxCzMmvkXpKDwQ6" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br />      
+      <img src="https://chocolate-cooperative-herring-689.mypinata.cloud/ipfs/QmRohbyBXkoCLNZuLmLK2zsfTVtyjvCoiL686CP8jc5DZb" alt="Italian Trulli"></img>
     </main>
   );
 }
 
 export default App;
+
+
