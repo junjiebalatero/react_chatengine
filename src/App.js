545949f23cdf72c441efa11541dcc935dff672be
junjiebalatero <junjiebalatero@gmail.com>
2023-10-23 23:31:41 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import "./App.css";
 function App() {  
   return (
     <main>
-      <h3>mardzreal57 web3 website hosted for free by 4everland.org </h3>
+      <h3>My All </h3>
       <iframe width="560" height="315" src="https://chocolate-cooperative-herring-689.mypinata.cloud/ipfs/QmU71yCyMg95f4HcBFL293DZphmJRtmyxCzMmvkXpKDwQ6" title="DSocial video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <br />      
+      <br />    
+      <h3>My All </h3>  
       <img src="https://chocolate-cooperative-herring-689.mypinata.cloud/ipfs/QmRohbyBXkoCLNZuLmLK2zsfTVtyjvCoiL686CP8jc5DZb" alt="Italian Trulli"></img>
     </main>
   );
