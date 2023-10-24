890c4adcb8a6ec138e9b7b8fa807bd47f792e0d4
junjiebalatero <junjiebalatero@gmail.com>
2023-10-24 12:08:01 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -1,46 +1,25 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-function App() {  
+function App() {
   return (
-    <main>
-        <h3> Just A Few List of Web 3.0</h3>          
-         
-          <div>
-            <p>Dsocial: <a href="https://dwqte-viaaa-aaaai-qaufq-cai.ic0.app/@mardz" target="_blank">Same as Youtube</a></p>
-          </div>
-          <div>
-            <p>Nuance: <a href="https://exwqn-uaaaa-aaaaf-qaeaa-cai.ic0.app/" target="_blank">Blogging to the People.</a></p>
-          </div>         
-          <div>
-            <p>Commonwealth.im: <a href="https://commonwealth.im/" target="_blank">A community for every token.</a></p>
-          </div>         
-          <div>
-            <p>distrikt: <a href="https://az5sd-cqaaa-aaaae-aaarq-cai.ic0.app/" target="_blank">Own your data and identity, make new connections, and build a community.</a></p>
-          </div>         
-          <div>
-            <p>DSCVR: <a href="https://dscvr.one/" target="_blank">a decentralized social content platform</a></p>
-          </div>
-          <div>
-            <p>Dstar Note: <a href="https://nazwh-uiaaa-aaaai-qbexq-cai.ic0.app/#/login" target="_blank">all data of the notes is stored in the blockchain</a></p>
-          </div>
-          <div>
-            <p>Dmail: <a href="https://evyc3-ziaaa-aaaak-aam5a-cai.ic0.app/login" target="_blank">Decentralize email</a></p>
-          </div>
-          <div>
-            <p>dbox: <a href="https://2prn7-tqaaa-aaaai-acjja-cai.ic0.app/login" target="_blank">Decentralize email</a></p>
-          </div>
-          <div>
-            <p>Visit tribes.xyz: <a href="https://www.tribes.xyz/" target="_blank">Decentralize Chat. </a></p>
-          </div>
-          <div>
-            <p>Visit oc.app: <a href="https://oc.app/" target="_blank">Decentralize Chat. </a></p>
-          </div>          
-    </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
 export default App;
-
-
