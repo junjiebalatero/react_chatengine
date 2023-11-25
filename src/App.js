1ccc024b2947ee1306729cf6df6340831c8da5e7
junjiebalatero <junjiebalatero@gmail.com>
2023-11-25 16:19:45 +0800

    ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ function App() {
   return (
       <div>
         Hello JUnjie
-      </div>
+        <h2>Search A Song or Artist Here:</h2>
+           <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for a song.." title="Type in a song"></input>
+<a class="accordion-toggle" href="#content-a">
+  1 - Simply Jessie by Rex Smith</a>
+ class="accordion" id="content-a" 
+<pre>           
+<iframe width="560" height="315" src="https://www.youtube.com/embed/LOSl_y8wVgM?si=Nz1JamDfBUYysPIv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+</pre>
+      </div>      
   );
 }
 
